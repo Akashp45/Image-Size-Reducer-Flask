fc49245f3905d560c0d704737c966ac6c274a3a6
Akashp45 <122898495+Akashp45@users.noreply.github.com>
2024-12-15 20:41:59 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -58,9 +58,18 @@ scheduler.start()
 # 	atexit.register(lambda:scheduler.shutdown())
 
 # main driver function
+# if __name__ == '__main__':
+#     if not os.path.exists(os.getcwd() + "/uploads"):
+#         os.mkdir('uploads')
+#     app.run(host='0.0.0.0', port=5000, debug=True)
+#     atexit.register(lambda: scheduler.shutdown())
+
+
 if __name__ == '__main__':
     if not os.path.exists(os.getcwd() + "/uploads"):
         os.mkdir('uploads')
-    app.run(host='0.0.0.0', port=5000, debug=True)
-    atexit.register(lambda: scheduler.shutdown())
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
 
